94f693750b2967ac3ead7bf6ab43c4a1e6bb4913
JeffreyDinackus <jeffreydinackus@proton.me>
2023-07-19 14:04:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Navbar = () => {
+  return (<nav
+  className="relative flex w-full flex-wrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-4">
+  <div className="flex w-full flex-wrap items-center justify-between px-3">
+    <div class="ml-2">
+      <p className="text-xl text-neutral-800 dark:text-neutral-200">Weather.JeffreyDinackus.com</p
+      >
+    </div>
+  </div>
+</nav>
+);
+
+}
+
+
+function ApiKeys() {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline"> xd</p>
+      <input type="text" />
+    </div>);
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <ApiKeys></ApiKeys>
     </div>
   );
 }
