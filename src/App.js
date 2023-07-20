9dd1b7f3d4a504f45ad47d3f52837d24758427c1
JeffreyDinackus <jeffreydinackus@proton.me>
2023-07-20 15:45:06 -0400

    no change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import React, { useState } from 'react';
 
 
 
+function Navbar() {
+  return (<nav
+  className="relative flex w-full flex-wrap items-center justify-between bg-[#FBFBFB] py-2 text-black shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-red-600 lg:py-4">
+  <div className="flex w-full flex-wrap items-center justify-between px-3">
+    <div class="ml-2">
+      <p className="text-xl text-white dark:text-slade-600"><a href="https://www.weather.JeffreyDinackus.com">Weather.JeffreyDinackus.com</a></p
+      >
+    </div>
+  </div>
+</nav>);}
+
+
 const ApiKeyIntakeForm = () => {
   const [apiKey, setApiKey] = useState('');
 
@@ -41,19 +53,6 @@ const ApiKeyIntakeForm = () => {
     </form></div>
   );
 };
-
-
-
-function Navbar() {
-  return (<nav
-  className="relative flex w-full flex-wrap items-center justify-between bg-[#FBFBFB] py-2 text-black shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-red-600 lg:py-4">
-  <div className="flex w-full flex-wrap items-center justify-between px-3">
-    <div class="ml-2">
-      <p className="text-xl text-white dark:text-slade-600"><a href="https://www.weather.JeffreyDinackus.com">Weather.JeffreyDinackus.com</a></p
-      >
-    </div>
-  </div>
-</nav>);}
 
 
 function App() {
